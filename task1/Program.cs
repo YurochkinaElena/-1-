f937ad07c3c89aa4f771fc9755364aec3906f34b
yurochkina.elena <transnab@mail.ru>
2022-11-12 23:47:18 +0300

    Задача 3

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number2 * number2;
+if (kvadrat == number1)
+{
+    Console.WriteLine("Второе число является корнем первого");
+} 
+else
+{
+    Console.WriteLine("Второе число не является корнем первого");
+}
