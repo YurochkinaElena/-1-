2523478ee6ed60eec6c70e538f30a1a047816137
yurochkina.elena <transnab@mail.ru>
2022-11-13 16:49:16 +0300

    ДЗ задача 2

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) 
+    Console.WriteLine("Число 1 больше числа 2");
+else
+Console.WriteLine("Число 2 больше числа 1");
