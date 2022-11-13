c6c3034e90430729e360545dd53504b47f5a46c9
yurochkina.elena <transnab@mail.ru>
2022-11-13 17:52:21 +0300

    ДЗ задача 6

---
+++ task7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+if (b == 0)
+{
+    Console.Write("Число является четным");
+}
+else
+{
+    Console.Write("Число не является четным");
+}
