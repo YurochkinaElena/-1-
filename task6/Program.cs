6f4002f644a5d0122a4f9853c584c4839b567d8e
yurochkina.elena <transnab@mail.ru>
2022-11-13 17:14:13 +0300

    ДЗ задача 4

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите number3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.WriteLine(max);
